39ad5e4eaba6428ae008d02a0b5014e66fc2e8dc
Trilochan-Reddy <trilochanreddy100@gmail.com>
2023-02-15 15:33:22 -0600

    updated files

--- javascript/demo.js
+++ javascript/demo.js
@@ -2,9 +2,16 @@ let heading=document.getElementById("headingElement");
 heading.textContent="4.0 Technologies";
 heading.style.color="blue";
 console.log(heading.textContent);
+let inputName=document.getElementById("inputNameElement");
+console.log(inputName.value);
 
 function a(){
     console.log("hello trilochan u clicked change heading button");
     heading.textContent="Text Changed";
     heading.style.color="red";
+}
+function signInText(){
+    let inputValue=inputName.value;
+    let signInTextValue=document.getElementById("signinText");
+    signInTextValue.textContent="Hi "+inputValue+" Verifying your account.";
 }
---
+++ javascript/randomNumber.js
@@ -0,0 +1,6 @@
+console.log(Math.ceil(Math.random() * 100));
+
+
+
+// Math.ceil()-->rounds up to next largest number
+// Math.random()-->generates random number(float number) in range of 0 to less than 1
