1c3afd56e74d7d114d429a1a326746fc723fee47
Trilochan-Reddy <trilochanreddy100@gmail.com>
2023-02-15 15:06:14 -0600

    Update counterApplication.js

--- javascript/counterApplication.js
+++ javascript/counterApplication.js
@@ -4,30 +4,53 @@ console.log(typeof(counterValueElement.textContent));
 let countDecreaseButton=document.getElementById("countDecrease");
 let countResetButton=document.getElementById("countReset");
 let countIncreaseButton=document.getElementById("countIncrease");
-let count=parseInt(counterValueElement.textContent);
+
 
 
 function decreaseValue(){
     
     // console.log("D B C");
-    count=count-1;
+    let previousCountValue=parseInt(counterValueElement.textContent);
+    let updatedCountValue=previousCountValue-1;
     // console.log((count));
     // console.log(typeof(count));
-    counterValueElement.textContent=count;
+    counterValueElement.textContent=updatedCountValue;
+
+    if (updatedCountValue>0){
+        counterValueElement.style.color="green";
+    }
+    else if (updatedCountValue<0) {
+        counterValueElement.style.color="red";
+    }
+    else{
+        counterValueElement.style.color="black";
+    }
 }
 
 function resetValue(){
     // console.log("R B C");
-    count=0;
+    let updatedCountValue=0;
     // console.log((count));
     // console.log(typeof(count));
-    counterValueElement.textContent=count;
+    counterValueElement.textContent=updatedCountValue;
+    counterValueElement.style.color="black";
 }
 
 function increaseValue(){
     // console.log("I B C");
-    count=count+1;
-    // console.log((count));
-    // console.log(typeof(count));
-    counterValueElement.textContent=count;
+    let previousCountValue=parseInt(counterValueElement.textContent);
+    let updatedCountValue=previousCountValue+1;
+    counterValueElement.textContent=updatedCountValue;
+
+    if (updatedCountValue>0){
+        counterValueElement.style.color="green";
+    }
+    else if (updatedCountValue<0) {
+        counterValueElement.style.color="red";
+    }
+    else{
+        counterValueElement.style.color="black";
+    }
+   
+    
 }
