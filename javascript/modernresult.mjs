e5d9fb6063ce9ebea89f0d7eb777269632f1d925
Trilochan Reddy Sama <trilochanreddy100@gmail.com>
2023-05-09 16:29:58 -0500

    added ES6 calculator and result.mjs files
    added ES6 calculator and result.mjs files

---
+++ javascript/modernresult.mjs
@@ -0,0 +1,3 @@
+import add from"./moderncalculator.mjs"
+
+console.log(add(2,3));
--- javascript/result.js
+++ javascript/result.js
@@ -1,3 +1,6 @@
-const add=require("./calculator");
+// const calculator=require("./calculator");
+const {add,sub}=require("./calculator");
+// const {add,sub} =calculator;
 
 console.log(add(2,3));
+console.log(sub(2,3));
